bbfde1255f1cc7d14c2d82e190a1410746f0d72e
CHNTheSky <76511295+CHNTheSky@users.noreply.github.com>
2021-12-25 15:45:42 +0800

    Update dma_wrapper.scala

--- dma_wrapper.scala
+++ dma_wrapper.scala
@@ -1,5 +1,9 @@
 import spinal.core._
 import spinal.lib._
+case class portA(Datawidthout : Int) extends Bundle{
+  val data=Bits(8 bits)
+  val dataout=Bits(Datawidthout bits)
+}
 case class DmaWrapper(Datawidthin : Int,Datawidthout : Int) extends Component{
   val io = new Bundle{
     val axis_tkeep =in Bits(Datawidthin/8 bits)
@@ -10,10 +14,9 @@ case class DmaWrapper(Datawidthin : Int,Datawidthout : Int) extends Component{
   val datavec=io.axis.payload.subdivideIn(8 bits)//切片
 
   val fifocach=StreamFifo(
-    dataType = Bits(8 bits),
+    dataType = portA(Datawidthout : Int),
     depth = Datawidthout/8
   )
-
   def isone(bit : Int,data: Bits)={ //根据keep判断哪些字节有效
     val isone = False
       when(data(bit)){
@@ -24,47 +27,13 @@ case class DmaWrapper(Datawidthin : Int,Datawidthout : Int) extends Component{
   when(io.axis.fire){
     for(i<-0 to datavec.length){
       when(isone(i,io.axis_tkeep)){
-        fifocach.io.push<-/<Stream(datavec.read(i))
+        fifocach.io.push<-/<Stream(datavec.read(i).asBits)
       }
     }
   }
   when(fifocach.io.occupancy === fifocach.io.occupancy.maxValue){
-    io.dmaWrapper<-/<fifocach.io.pop
+    io.dmaWrapper<-/<Stream(fifocach.io.pop.dataout)
   }
-/*
 
-case class portA() extends Bundle{
-  val data=UInt(512 bits)
-  val mty=UInt(6 bits)
 }
-case class portB() extends Bundle{
-  val data=UInt(512 bits)
-  val tkeep=UInt(64 bits)
-}
-case class busAdapt(fifoDepth:Int=512) extends Component {
-  val io=new Bundle{
-    val dataIn=slave Stream(portA())
-    val dataOut=master Stream(portB())
-  }
-  noIoPrefix()
-  val _=new Area{
-    val fifoCache=StreamFifo(portA(),fifoDepth)
-    val busCov=portB()
-    if(clockDomain.config.resetActiveLevel==HIGH)
-      fifoCache.io.flush:=clockDomain.readResetWire
-    else
-      fifoCache.io.flush:= !clockDomain.readResetWire
-    io.dataIn<>fifoCache.io.push
-    busCov.data:=fifoCache.io.pop.data
-    keepCov.mty2keep(busCov.tkeep,fifoCache.io.pop.mty)
-    io.dataOut<>fifoCache.io.pop.translateWith(busCov)
-  }.setName("")
-}
-object keepCov{
-  def mty2keep(tkeep:UInt,mty:UInt)={
-    val mtyIndex=for(index<-0 to 63) yield U((BigInt(1)<<(64-index))-1)
-    tkeep:=mtyIndex.read(mty)
-  }
-}
-*/
-}
+
