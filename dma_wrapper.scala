1811297b7f16a1c094e52d69ddf1a406eb19dd05
CHNTheSky <76511295+CHNTheSky@users.noreply.github.com>
2021-12-27 08:20:33 +0800

    Update dma_wrapper.scala

--- dma_wrapper.scala
+++ dma_wrapper.scala
@@ -6,30 +6,32 @@ case class DmaWrapper(dataWidth: Int) extends Component{
     val axis = slave Stream (Bits(dataWidth bits))
     val dmaWrapper = master Stream (Bits(dataWidth bits))
   }
-  def firstOne(data:Bits)={
+  def firstOne(data:Bits):UInt={
     val index = UInt
     for(i<-0 until io.axis_tkeep.getWidth){
-    when(data(i)=/=0){
+    when(data(i).asUInt===1){
       index := i
     }
   }
     index
   }
-  def lastOne(data:Bits)={
+  def lastOne(data:Bits):UInt={
     val index = UInt
     for(i<-0 until io.axis_tkeep.getWidth){
-      when(data(i)===0){
+      when(data(i).asUInt===0){
         index := i
       }
     }
     index
   }
-  val wantedId = Counter(1, inc = io.axis.fire)
+  val wantedId = Counter(2, inc = io.axis.fire)
   val twoStreams = StreamDemux(io.axis, select = wantedId, portCount = 2)
   val validData1 = twoStreams(0)
   val validData2 = twoStreams(1)
   io.dmaWrapper<<StreamJoin.arg(validData1,validData2).translateWith{
-    (validData1.payload.rotateLeft(0)>>firstOne(validData1.payload).rotateLeft(0))## (validData2.payload>>lastOne(validData2.payload))
+    ((validData1.payload.rotateLeft(0)>>firstOne(validData1.payload).rotateLeft(0))##
+      (validData2.payload>>lastOne(validData2.payload))).resized
   }
 
 }
+
