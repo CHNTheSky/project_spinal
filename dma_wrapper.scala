f3ee519326727cd542c3f480160d1207a1863a55
CHNTheSky <76511295+CHNTheSky@users.noreply.github.com>
2021-12-29 15:35:27 +0800

    Update dma_wrapper.scala

--- dma_wrapper.scala
+++ dma_wrapper.scala
@@ -1,13 +1,45 @@
 import spinal.core._
 import spinal.lib._
-case class DmaWrapper(dataWidth: Int) extends Component{
+import scala.Int
+case class DmaWrapper(busWidth: Int,dataOutWidth: Int) extends Component{
   val io = new Bundle {
-    val axis_tkeep = in Bits (dataWidth / 8 bits)
-    val axis = slave Stream (Bits(dataWidth bits))
-    val dmaWrapper = master Stream (Bits(dataWidth bits))
+    val axis_tkeep = in Bits (busWidth / 8 bits)
+    val axis_tlast = in Bool
+    val axis = slave Stream (Bits(busWidth bits))
+    val dmaWrapper = master Stream (Bits(busWidth bits))
   }
-  def firstOne(data:Bits):UInt={
-    val index = UInt
+  io.dmaWrapper.m2sPipe()//打拍
+  val fifoCach=StreamFifo(
+    dataType = Bits(busWidth bits),
+    depth = 8
+  )
+  val isFirstOne = RegNext(io.axis_tlast) init(True)
+  val dataJoin = Reg(Bits(dataOutWidth bit))
+  val shiftBit = Reg(UInt())
+
+
+  val logic_in = new Area{
+    fifoCach.io.push<<io.axis
+    when(isFirstOne){
+      shiftBit:=firstValid(io.axis.payload)
+    }
+  }
+
+
+  val logic_out = new Area{
+    val streamOut=Stream(Bits(dataOutWidth bits))
+    when(io.axis_tlast){
+      streamOut.valid:=True
+    }.otherwise(streamOut.valid:=False)
+    val dataAdapt=Stream(Bits(busWidth bits))
+    dataJoin := dataJoin##fifoCach.io.pop.payload//拼接
+    io.dmaWrapper<>StreamWidthAdapter.make(streamOut.translateWith((dataJoin<<(shiftBit*8)).resizeLeft(dataOutWidth)).m2sPipe(),Bits(busWidth bits)).queue(10)
+
+  }
+
+
+  def firstValid(data:Bits)={
+    val index = U(0)
     for(i<-0 until io.axis_tkeep.getWidth){
     when(data(i).asUInt===1){
       index := i
@@ -15,23 +47,7 @@ case class DmaWrapper(dataWidth: Int) extends Component{
   }
     index
   }
-  def lastOne(data:Bits):UInt={
-    val index = UInt
-    for(i<-0 until io.axis_tkeep.getWidth){
-      when(data(i).asUInt===0){
-        index := i
-      }
-    }
-    index
-  }
-  val wantedId = Counter(2, inc = io.axis.fire)
-  val twoStreams = StreamDemux(io.axis, select = wantedId, portCount = 2)
-  val validData1 = twoStreams(0)
-  val validData2 = twoStreams(1)
-  io.dmaWrapper<<StreamJoin.arg(validData1,validData2).translateWith{
-    ((validData1.payload.rotateLeft(0)>>firstOne(validData1.payload).rotateLeft(0))##
-      (validData2.payload>>lastOne(validData2.payload))).resized
-  }
 
 }
+
 
