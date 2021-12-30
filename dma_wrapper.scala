58c54f532f8f47d0be4166ff9f541d4c3cfca2a2
CHNTheSky <76511295+CHNTheSky@users.noreply.github.com>
2021-12-30 13:01:55 +0800

    Update dma_wrapper.scala

--- dma_wrapper.scala
+++ dma_wrapper.scala
@@ -11,38 +11,44 @@ case class DmaWrapper(busWidth: Int,dataOutWidth: Int) extends Component{
     dataType = Bits(busWidth bits),
     depth = 8
   )
-  val isFirstOne = RegNext(io.axis_tlast) init(True)
+  val isFirstOne = RegNext(io.axis_tlast) init(True)//第一拍标志
   val dataJoin = Reg(Bits(dataOutWidth bit))
+  val tmpdata = Reg(Bits(8*dataOutWidth bit))//最大的8拍拼接
   val shiftBit = Reg(UInt())
-  val thisStage = Reg(UInt(10 bits)) init(1)
+  val lastStage = Reg(UInt(3 bits)) init(0)//上一拍标志
+  val curStage= RegNext(lastStage)//当前拍标志
 
-  val thisstage  =new Area{
+  val laststage  =new Area{
     when(isFirstOne){
-     thisStage:=1
-    }.elsewhen(thisStage===0){
-      thisStage:=0//最后一拍为0
-    }.otherwise(thisStage:=thisStage+1)
+      lastStage:=0
+    }.otherwise(lastStage:=lastStage+1)
   }
 
   val logic_in = new Area{
     fifoCach.io.push<<io.axis
-    when(thisStage===1){
+    when(isFirstOne){
       shiftBit:=firstValid(io.axis.payload)
     }
   }
-
-
   val logic_out = new Area{
     val streamOut=Stream(Bits(dataOutWidth bits))
     when(io.axis_tlast){
       streamOut.valid:=True
     }.otherwise(streamOut.valid:=False)
     fifoCach.io.pop.ready:=True
-    dataJoin := dataJoin(0 until busWidth)##fifoCach.io.pop.payload//拼接
+    for(i<-0 until 7){
+      if(i == 1){
+        when(fifoCach.io.pop.valid === True) {
+          tmpdata := fifoCach.io.pop.payload.resized
+        }
+      }
+      when(fifoCach.io.pop.valid === True && lastStage<=curStage){//是当前的这一拍
+        tmpdata:= (tmpdata(i*busWidth-1 downto 0)##fifoCach.io.pop.payload).resized//拼接
+      }
+    }
+    dataJoin:=tmpdata.resized
     io.dmaWrapper<>StreamWidthAdapter.make(streamOut.translateWith((dataJoin<<(shiftBit*8)).resizeLeft(dataOutWidth)).m2sPipe(),Bits(busWidth bits)).queue(10)
-
   }
-
   def firstValid(data:Bits)={
     val index = U(0)
     for(i<-0 until io.axis_tkeep.getWidth){
@@ -53,6 +59,5 @@ case class DmaWrapper(busWidth: Int,dataOutWidth: Int) extends Component{
     index
   }
 }
-
 
 
